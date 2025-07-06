757f2c8ed8e2edbc6c0cba728d55676b1d27603e
Axil Rais <axil@MacBook-Pro-2.local>
2025-07-05 20:27:28 -0400

    Authentication functionality

---
+++ app/src/main/java/com/example/realmail/HomeActivity.java
@@ -0,0 +1,58 @@
+package com.example.realmail;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class HomeActivity extends AppCompatActivity {
+
+    protected Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        toolbar = findViewById(R.id.hometoolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.cam_on);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.camera){
+
+        }
+
+        if(item.getItemId() == R.id.settings){
+            Intent intent = new Intent(HomeActivity.this, SettingsActivity.class);
+            startActivity(intent);
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/realmail/NewAccount.java
@@ -0,0 +1,73 @@
+package com.example.realmail;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class NewAccount extends AppCompatActivity {
+
+    protected Button submit;
+    protected EditText email, password, cpassword;
+
+    private FirebaseAuth auth;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_new_account);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        auth = FirebaseAuth.getInstance();
+
+        submit = findViewById(R.id.submit);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        cpassword = findViewById(R.id.cpassword);
+
+
+        submit.setOnClickListener(v -> {
+            createAccount();
+        });
+    }
+
+    private void createAccount(){
+        String email1 = email.getText().toString();
+        String password1 = password.getText().toString();
+        String cpassword1 = cpassword.getText().toString();
+
+        if(!password1.equals(cpassword1)){
+            Toast.makeText(this, "Passwords don't match!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        auth.createUserWithEmailAndPassword(email1, password1).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()){
+                Toast.makeText(this, "Account created successfully!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(NewAccount.this, MainActivity.class);
+                startActivity(intent);
+            }
+            else {
+                Toast.makeText(this, "Error creating account", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        });
+
+    }
+}
