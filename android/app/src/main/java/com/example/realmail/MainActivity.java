a9a070fb2c33afe6b3472170527ee424e5cfc138
GDR12345 <daranguo123@gmail.com>
2025-07-22 09:14:56 -0400

    Daran Setting Page Update Jul 22

--- android/app/src/main/java/com/example/realmail/MainActivity.java
+++ android/app/src/main/java/com/example/realmail/MainActivity.java
@@ -40,16 +40,14 @@ public class MainActivity extends AppCompatActivity {
     private final ActivityResultLauncher<String> requestPermissionLauncher =
             registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
                 if (isGranted) {
-                    // FCM SDK (and your app) can post notifications.
+                    //push notification
                 } else {
-                    // Inform user that your app will not show notifications.
+                    //notification not shown
                 }
             });
     protected Button signIn;
     protected TextView forgotpw, newaccount;
     protected EditText email, password;
-    // declaration below for allow notifications
-    // see https://firebase.google.com/docs/cloud-messaging/android/client
     String global_device_registration_token = "";
     private FirebaseAuth auth;
 
@@ -69,20 +67,11 @@ public class MainActivity extends AppCompatActivity {
         email = findViewById(R.id.email_sign_in);
         password = findViewById(R.id.password_sign_in);
 
-        // Ask for notification permission if necessary
         askNotificationPermission();
 
-        // Create notification channel
-        // ref: https://developer.android.com/develop/ui/views/notifications/channels
         createNotificationChannel();
 
         auth = FirebaseAuth.getInstance();
-
-        //go to home page if user is already logged in
-       /* if (auth.getCurrentUser() != null){
-            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-            startActivity(intent);
-        }*/
 
         signIn.setOnClickListener(v -> {
             signIn();
@@ -102,23 +91,21 @@ public class MainActivity extends AppCompatActivity {
     private void signIn() {
         auth = FirebaseAuth.getInstance();
 
-        //extract the email & pw
         String email1 = email.getText().toString();
         String password1 = password.getText().toString();
 
         auth.signInWithEmailAndPassword(email1, password1).addOnCompleteListener(this, task -> {
             if (task.isSuccessful()) {
-                // Get new FCM registration token
-                // https://firebase.google.com/docs/cloud-messaging/android/client#retrieve-the-current-registration-token
-                getDeviceRegistrationToken(); // Retrieve the current FCM registration token
+
+                getDeviceRegistrationToken();
                 String userEmail = task.getResult().getUser().getEmail();
                 new Thread(() -> {
-                    // Async Send the registration token to server
+
                     try {
                         sendRegistrationToServer(global_device_registration_token, userEmail);
                     } catch (Exception e) {
                         e.printStackTrace();
-                        // Handle exceptions, such as network errors or JSON parsing errors
+
                         runOnUiThread(() -> Toast.makeText(MainActivity.this, "Error sending registration token: " + e.getMessage(), Toast.LENGTH_LONG).show());
                     }
                 }).start();
@@ -134,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getDeviceRegistrationToken() {
-        // This method is used to retrieve the current FCM registration token
+
         FirebaseMessaging.getInstance().getToken()
                 .addOnCompleteListener(new OnCompleteListener<String>() {
                     @Override
@@ -143,20 +130,14 @@ public class MainActivity extends AppCompatActivity {
                             Log.w(TAG, "Fetching FCM registration token failed", task.getException());
                             return;
                         }
-                        // Get new FCM registration token
+
                         global_device_registration_token = task.getResult();
                     }
                 });
     }
 
     private void sendRegistrationToServer(@NonNull String token, @NonNull String email) throws Exception {
-        // send token to server via api call
-        // This method should be implemented to send the FCM token to your server
-        // For example, you can use Retrofit or any HTTP client to make a POST request
-        // to your server's endpoint that handles token registration.
 
-        // how to access device registration token
-        // https://firebase.google.com/docs/cloud-messaging/android/client#sample-register
         FirebaseMessaging.getInstance().getToken()
                 .addOnCompleteListener(new OnCompleteListener<String>() {
                     @Override
@@ -166,7 +147,6 @@ public class MainActivity extends AppCompatActivity {
                             return;
                         }
 
-                        // Get new FCM registration token
                         String token_ = task.getResult();
                         new Thread(() -> {
                             try {
@@ -174,9 +154,8 @@ public class MainActivity extends AppCompatActivity {
                                 URL url = new URL(stringURL);
                                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                                 connection.setRequestMethod("POST");
-                                connection.setDoOutput(false); // post request via params
+                                connection.setDoOutput(false);
                                 int responseCode = connection.getResponseCode();
-                                // below read the response from the server
                                 BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                                 String inputLine;
                                 StringBuilder response = new StringBuilder();
@@ -184,11 +163,9 @@ public class MainActivity extends AppCompatActivity {
                                     response.append(inputLine);
                                 }
                                 in.close();
-                                // Optionally log or use the response
                                 connection.disconnect();
                             } catch (Exception e) {
                                 e.printStackTrace();
-                                // Handle exceptions, such as network errors or JSON parsing errors
                                 Log.w("Main.Activity", "Error sending registration token to server: " + e.getMessage());
                             }
                         }).start();
@@ -198,13 +175,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
     private void createNotificationChannel() {
-        // // ref: https://developer.android.com/develop/ui/views/notifications/channels
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is not in the Support Library.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            String CHANNEL_ID = "default_channel_id"; // Define your channel ID
-            CharSequence name = getString(R.string.channel_name); // Add channel_name to strings.xml
-            String description = getString(R.string.channel_description); // Add channel_description to strings.xml
+            String CHANNEL_ID = "default_channel_id";
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
             channel.setDescription(description);
@@ -214,18 +188,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void askNotificationPermission() {
-        // This is only necessary for API level >= 33 (TIRAMISU)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
                     PackageManager.PERMISSION_GRANTED) {
-                // FCM SDK (and your app) can post notifications.
             } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
-                // TODO: display an educational UI explaining to the user the features that will be enabled
-                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
-                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
-                //       If the user selects "No thanks," allow the user to continue without notifications.
+
             } else {
-                // Directly ask for the permission
+
                 requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
             }
         }
